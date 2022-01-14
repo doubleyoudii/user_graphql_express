f0994bbd764c86eb160123961a9d99dceb9fa245
William Ducta <wducta@yondu.com>
2022-01-14 13:32:38 +0800

    CHORE: Fix format

--- models/mongo/user.model.js
+++ models/mongo/user.model.js
@@ -1,40 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const SALT_WORK_FACTOR = 10;
 
-
-const schema = new Schema({
+const schema = new Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-schema.pre('save', async function save(next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-        this.password = await bcrypt.hash(this.password, salt);
-        return next();
-    } catch (err) {
-        return next(err);
-    }
+schema.pre("save", async function save(next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 schema.methods.validatePassword = async function validatePassword(data) {
-    return bcrypt.compare(data, this.password);
+  return bcrypt.compare(data, this.password);
 };
 
-
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model("User", schema);
